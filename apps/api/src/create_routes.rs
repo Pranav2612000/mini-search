ea2e9b57d35aef76b4fa9d04768fd79ff2ff2f32
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-12-30 10:02:35 +0530

    fix api: disable cors

--- apps/api/src/create_routes.rs
+++ apps/api/src/create_routes.rs
@@ -1,7 +1,11 @@
+use std::vec;
+
 use axum::{routing::{get, post}, Router};
+use hyper::Method;
 use routes::health_check::health_check;
 use routes::trigger_indexing::trigger_indexing;
 use routes::search::search;
+use tower_http::cors::CorsLayer;
 
 use crate::routes;
 
@@ -16,4 +20,10 @@ pub fn create_router() -> Router {
     .route("/api/health", get(health_check))
     .route("/api/index/trigger", post(trigger_indexing))
     .route("/api/search", get(search))
+    .layer(
+      CorsLayer::new()
+        .allow_origin(tower_http::cors::Any)
+        .allow_methods(vec![Method::GET, Method::POST, Method::OPTIONS])
+        .allow_headers(vec![hyper::header::CONTENT_TYPE]),
+    )
 }
