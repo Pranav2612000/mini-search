379db3662f78f7710645a2e8be72685f786235e8
Pranav2612000 <pranav2000joglekar@gmail.com>
2025-01-02 08:45:25 +0530

    feat search-ui: allow controlling search using query params'

--- apps/search-ui/src/pages/Search.tsx
+++ apps/search-ui/src/pages/Search.tsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { MouseEvent } from "react";
 import { SearchResult } from "../types/Search";
 import fetchSearchResults from "../api/fetchSearchResults";
+import { useSearchParams } from "react-router-dom";
 
 const SearchPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [searchText, setSearchText] = React.useState('');
   const [isLoading, setIsLoading] = React.useState(false);
   const [searchResults, setSearchResults] = React.useState<SearchResult[]>([]);
   const [searchDuration, setSearchDuration] = React.useState(0);
-  const [isSearchCompleted, setIsSearchCompleted] = React.useState(false);
+  const searchQuery = searchParams.get('q') || "";
+  const isSearchCompleted = Boolean(searchQuery)
 
   const search = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setIsLoading(true);
-    setIsSearchCompleted(true);
-    const {results, took_ms: duration} = await fetchSearchResults(searchText);
-    setIsLoading(false);
-    setSearchResults(results);
-    setSearchDuration(duration);
+    setSearchParams({ q: searchText });
   };
 
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+
+    const populateSearchResults = async () => {
+      setIsLoading(true);
+      const {results, took_ms: duration} = await fetchSearchResults(searchQuery);
+      setIsLoading(false);
+      setSearchResults(results);
+      setSearchDuration(duration);
+    };
+    populateSearchResults();
+  }, [searchQuery]);
+
   return (
-    <div className={!isSearchCompleted ? "search-centered" : ""}>
+    <div className={`search-page ${!isSearchCompleted ? "search-centered" : ""}`}>
       <h1>Mini Search</h1>
 
       <form>
@@ -39,15 +52,21 @@ const SearchPage = () => {
       {isLoading && <p>Loading...</p>}
       {isSearchCompleted && !isLoading && (
         <div className="search-results">
-          <span>Took {searchDuration}ms</span>
           {searchResults.length === 0 && <p>No results found</p>}
           {searchResults.length > 0 && (
             <ul>
               {searchResults.map((result, index) => (
-                <li key={index}>{result.title}</li>
+                <li key={index} className="search-result-entry">
+                  <div>
+                    <a href={result.url}>
+                      {result.title}
+                    </a>
+                  </div>
+                </li>
               ))}
             </ul>
           )}
+          <span>Took {searchDuration}ms</span>
         </div>
       )}
       <div className="view-analytics-container">
